7e35a18a122ec93fe3d646d41b26a92bd28b468a
Giselle Cristina Moreno Tavares - DATAPREVCE <giselle.tavares@dataprev.gov.br>
2018-05-14 12:29:44 -0300

    axios, fetch, ajax

--- ajax/server.js
+++ ajax/server.js
@@ -29,5 +29,22 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 // app.get('/teste', (req, res) => res.send(new Date))
 app.listen(8081, () => console.log('Executando...'))
